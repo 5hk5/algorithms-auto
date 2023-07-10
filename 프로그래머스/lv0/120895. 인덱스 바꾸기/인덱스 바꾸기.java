eb6c1bf7b41448f91f784c3390978db9bb845d76
5hk5 <104435274+5hk5@users.noreply.github.com>
2023-07-10 22:44:49 +0900

    [level 0] Title: 인덱스 바꾸기, Time: 0.03 ms, Memory: 77 MB -BaekjoonHub

--- 프로그래머스/lv0/120895. 인덱스 바꾸기/인덱스 바꾸기.java
+++ 프로그래머스/lv0/120895. 인덱스 바꾸기/인덱스 바꾸기.java
@@ -2,31 +2,38 @@ class Solution {
     public String solution(String s, int num1, int num2) {
         String newS = "";
         
-        String[] sArray = s.split("");
+//         String[] sArray = s.split("");
         
-        for (int i=0;i<sArray.length;i++){
-            if (num1 > num2) {
-                if (i == num1){
-                    newS += sArray[num2];
-                    continue;
-                } else if (i == num2) {
-                    newS += sArray[num1];
-                    continue;
-                }
-            }
+//         for (int i=0;i<sArray.length;i++){
+//             if (num1 > num2) {
+//                 if (i == num1){
+//                     newS += sArray[num2];
+//                     continue;
+//                 } else if (i == num2) {
+//                     newS += sArray[num1];
+//                     continue;
+//                 }
+//             }
             
-            if (num1 < num2) {
-                if (i == num2){
-                    newS += sArray[num1];
-                    continue;
-                } else if (i == num1) {
-                    newS += sArray[num2];
-                    continue;
-                }
-            }
+//             if (num1 < num2) {
+//                 if (i == num2){
+//                     newS += sArray[num1];
+//                     continue;
+//                 } else if (i == num1) {
+//                     newS += sArray[num2];
+//                     continue;
+//                 }
+//             }
             
-            newS += sArray[i];
-        }
+//             newS += sArray[i];
+//         }
+
+        char[] ch = s.toCharArray();
+
+        ch[num1] = s.charAt(num2);
+        ch[num2] = s.charAt(num1);
+
+        newS = String.valueOf(ch);
         
         return newS;
         
