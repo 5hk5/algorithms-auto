ff85c813295dcea3a5afff7b0aad2e81b96bdb47
5hk5 <104435274+5hk5@users.noreply.github.com>
2023-09-03 16:47:12 +0900

    [level 2] Title: 최댓값과 최솟값, Time: 8.77 ms, Memory: 72.6 MB -BaekjoonHub

--- 프로그래머스/lv2/12939. 최댓값과 최솟값/최댓값과 최솟값.java
+++ 프로그래머스/lv2/12939. 최댓값과 최솟값/최댓값과 최솟값.java
@@ -3,10 +3,11 @@ class Solution {
         //공백으로 구분된 문자열 (숫자들) => 최소값, 최대값 구하기 ("최솟값 최대값")     
         String[] sArray = s.split(" ");
         
-        int max = Integer.parseInt(sArray[0]);
-        int min = Integer.parseInt(sArray[0]);
+        int max, min;
+        max = min = Integer.parseInt(sArray[0]);
         for (String string : sArray){
             int num = Integer.parseInt(string);
+            
             min = Math.min(min, num);
             max = Math.max(max, num);
         }
