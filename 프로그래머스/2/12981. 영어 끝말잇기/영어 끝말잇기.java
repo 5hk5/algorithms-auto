905b797cbef53f2eca774b0fbbe8c33135e72cf7
5hk5 <104435274+5hk5@users.noreply.github.com>
2023-11-25 14:35:49 +0900

    [level 2] Title: 영어 끝말잇기, Time: 0.06 ms, Memory: 75.1 MB -BaekjoonHub

--- 프로그래머스/2/12981. 영어 끝말잇기/영어 끝말잇기.java
+++ 프로그래머스/2/12981. 영어 끝말잇기/영어 끝말잇기.java
@@ -3,21 +3,16 @@ import java.util.*;
 class Solution {
     public int[] solution(int n, String[] words) {
         int[] answer = new int[2];
-        List<String> wordsList = new ArrayList<String>();
-        wordsList.add(words[0]);
+        Set<String> wordsSet = new HashSet<>();
+        wordsSet.add(words[0]);
         
         for (int i=1;i<words.length;i++){            
-            // 단어 중복 체크
-            if (wordsList.contains(words[i])) {
+            if (wordsSet.contains(words[i]) || 
+               words[i-1].charAt(words[i-1].length()-1) != words[i].charAt(0)) {
                 return new int[] {i % n + 1, i / n + 1}; // 번호, 실패한 차례
             }
             
-            // 끝말잇기 실패 체크
-            if (words[i-1].charAt(words[i-1].length()-1) != words[i].charAt(0)) {
-                return new int[] {i % n + 1, i / n + 1};
-            }
-
-            wordsList.add(words[i]);
+            wordsSet.add(words[i]);
         }
         
         return answer;
