5db516ea9d7c5c2443223bf6c86ae883dce8bfbc
5hk5 <104435274+5hk5@users.noreply.github.com>
2024-03-09 15:26:56 +0900

    [Silver V] Title: 다리 놓기, Time: 148 ms, Memory: 14412 KB -BaekjoonHub

---
+++ 백준/Silver/1010. 다리 놓기/다리 놓기.java
@@ -0,0 +1,43 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+/* 서쪽 사이트 - 동쪽 사이트 잇는 다리 지을 때 경우의 수 (0<w<=e<30)
+ * 한 사이트당 하나의 다리만 지음, 서쪽 사이트 수만큼 다리 지음, 다리끼리 겹쳐질 수 없음
+ * => 조합 (eCw)
+ * 입력 1 : 테스트케이스 개수
+ * 입력 2 : w e (w: 서쪽 사이트 수)
+ */
+public class Main {
+
+    static int[][] dp = new int[30][30];
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st;
+
+        int T = Integer.parseInt(br.readLine());
+        for (int i=0;i<T;i++){
+            st = new StringTokenizer(br.readLine(), " ");
+
+            int w = Integer.parseInt(st.nextToken());
+            int e = Integer.parseInt(st.nextToken());
+
+            System.out.println(countBridges(e, w));
+        }
+    }
+
+    /* 조합 공식 활용 (eCw)
+     * eC0 = eCe = 1
+     * e+1Cw+1 = eCw + eCw+1
+     * */
+    private static int countBridges(int e, int w) {
+        if (dp[e][w] > 0) return dp[e][w]; // 배열에 저장된 값은 바로 반환
+
+        if (w == 0 || w == e) return dp[e][w] = 1; // 공식 1 활용
+
+        return dp[e][w] = countBridges(e-1, w-1) + countBridges(e-1, w); // 공식 2 활용
+    }
+
+}
